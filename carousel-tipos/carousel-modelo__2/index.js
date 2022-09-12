efb1665505d4bf363c9d79be17ab7cc3dc1b00a1
Jeffersondeab <82956161+Jeffersondeab@users.noreply.github.com>
2022-09-12 11:23:54 -0300

    add

---
+++ carousel-tipos/carousel-modelo__2/index.js
@@ -0,0 +1,4 @@
+const carouselList = document.querySelector('[data-carousel="list"]')
+const carouselItems = document.querySelectorAll('[data-carousel="item"]')
+const btnPrevious = document.querySelectorAll('[data-carousel="btn-previous"]')
+const btnNext = document.querySelectorAll('[data-carousel="btn-next"]')
